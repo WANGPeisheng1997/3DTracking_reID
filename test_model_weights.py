5f2f8e94e36f465ca898f0a25581a6318a29a367
王沛晟 <115681033@qq.com>
2019-04-29 19:19:15 +0800

    debug

--- test_model_weights.py
+++ test_model_weights.py
@@ -5,5 +5,8 @@ import os
 path = os.path.join('model', 'epoch_last.model')
 model = Model(751)
 print(model.model.conv1.weight)
+print(model.classifier.weight)
+
 model.load_state_dict(torch.load(path))
 print(model.model.conv1.weight)
+print(model.classifier.weight)
